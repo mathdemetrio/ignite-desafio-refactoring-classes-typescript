d05701cbc80f63e2026afddaeeb60d535b55befd
Matheus Faustino Demetrio <matheus.demetrio@dotgroup.com.br>
2022-05-15 19:38:13 -0300

    Refactoring modal add and edit food

---
+++ src/components/ModalEditFood/index.tsx
@@ -0,0 +1,53 @@
+import { useRef } from 'react';
+import { FiCheckSquare } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+
+import { Form } from './styles';
+import { Modal } from '../Modal';
+import { Input } from '../Input';
+
+interface IFood {
+  id: number;
+  name: string;
+  description: string; 
+  price: number;
+  image: string;
+  available: boolean;
+}
+
+interface ModalEditFood {
+  isOpen: boolean;
+  editingFood: IFood;
+  setIsOpen: () => void;
+  handleUpdateFood: (food: IFood) => void;
+}
+
+export function ModalEditFood({isOpen, setIsOpen, editingFood, handleUpdateFood}: ModalEditFood) {
+  const formRef = useRef<FormHandles>(null)
+
+  const handleSubmit = async (data: IFood) => {
+    handleUpdateFood(data);
+    setIsOpen();
+  };
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
+        <h1>Editar Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" />
+
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
+
+        <Input name="description" placeholder="Descrição" />
+
+        <button type="submit" data-testid="edit-food-button">
+          <div className="text">Editar Prato</div>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+};
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -2,8 +2,8 @@ import { Component } from 'react';
 
 import { Header } from '../../components/Header';
 import { Food } from '../../components/Food';
-import ModalAddFood from '../../components/ModalAddFood';
-import ModalEditFood from '../../components/ModalEditFood';
+import { ModalAddFood } from '../../components/ModalAddFood';
+import { ModalEditFood } from '../../components/ModalEditFood';
 import { FoodsContainer } from './styles';
 
 import api from '../../services/api';
